593666458044a56f8c1bbc528bc45996bce87104
Tb2019 <982148620@qq.com>
2024-06-07 14:05:42 +0800

    正常提交

--- 手动保存csv至数据库.py
+++ 手动保存csv至数据库.py
@@ -6,29 +6,29 @@ from asyncio.log import logger
 from utils import csv_2_df, truncate_table, df2mysql, drop_duplicate_collage, local_engine, sf_engine, save_as_json
 from urllib import parse
 
-school_id = 92
-college_id = 148
-school_name = '北京师范大学'
-college_name = '化学学院'
+school_id = 91
+college_id = 149
+school_name = '华东师范大学'
+college_name = '物理与电子科学学院'
 
-img_url_head = ''
+img_url_head = 'https://faculty.ecnu.edu.cn/'
 
 data_real = {
-"姓名":"于勐",
+"姓名":"田博博",
 "电话":"",
-"邮箱":"nkyu2023@nankai.edu.cn",
-"职称":"副教授",
-"个人简介":"于勐，博士，副教授，应用化学与工程研究所，毕业于南开大学，天津人。",
-"研究方向":"能源材料化学，电催化材料与器件",
-"专利":"获发明专利授权专利1项",
-"科研项目":"作为项目负责人或骨干成员承担国家自然科学基金青年项目、国家重点研发计划“催化科学”专项等科研任务。",
-"荣誉/获奖":"2022年天津市自然科学二等奖（第二完成人）\n第二十次全国电化学大会优秀论文奖\nJournal of Energy Chemistry2022年作者交流会最佳报告奖",
-"照片地址":"https://chem.nankai.edu.cn/_upload/article/images/…e78a505d/3263b494-6b8c-452d-9e90-c1fb364996f5.jpg",
+"邮箱":"bbtian@ee.ecnu.edu.cn",
+"职称":"教授",
+"个人简介":"田博博，物理与电子科学学院电子科学系教授，博士生导师。2016年获法国Paris-Saclay大学和中国科学院大学双博士学位（Ph.D.）。主讲本科专业基础课《半导体物理》、《人工智能导论》和《理论力学》，教学获第五届上海高校青年教师教学比赛优秀奖，第十三届华东师范大学青年教师教学比赛一等奖。指导教育部A类科创比赛获得国家级一等奖3项，二等奖1项，省部级一等奖1项，二等奖2项。\n研究方向为基于铁电材料的类脑智能器件。主持含国家自然基金（3项）和上海市项目（3项）在内的科研项目10余项，在Nat. Mater.、Nat. Electron.、Appl. Phys. Rev.、Nat. Commun.等国际重要期刊发表SCI论文100余篇。入选国家优青，中国博士后创新人才计划和中国科学院院长特别奖。担任中国神经科学学会类脑智能分会委员会委员，以及InfoMat (IF:22.7)、NML (IF:26.6)、IJEM (IF:14.7)、Exploration和Brain-X期刊的青年编委。",
+"研究方向":"依托上海类脑智能材料与器件研究中心和极化材料与器件教育部重点实验室，重点利用铁电信息材料开展类脑智能器件、存储、存算一体、感存算一体计算的研究，相关成果发表于Nat. Mater.、Nat. Electron.、Appl. Phys. Rev.、Nat. Commun.、InfoMat、Adv. Funct. Mater.、Adv. sci.等国际重要期刊。欢迎研究生和青年人才加入科研团队。",
+"专利":"",
+"科研项目":"主持含国家自然科学基金（3项）、省部级项目（5项）在内的科研项目10余项。入选国家级青年、省部级杰青和中国博士后创新等人才计划。",
+"荣誉/获奖":"教学：\n获得第五届上海高校青年教师教学比赛优秀奖，第十三届华东师范大学青年教师教学比赛一等奖，指导教育部A类科创比赛获得国家级一等奖3项，二等奖1项，省部级一等奖1项，二等奖2项。\n科研：\n中国科学院院长特别奖，中科院优博百篇，中国电子教育学会优博，中国博士后创新人才，上海市扬帆人才，上海市晨光人才，国家级青年人才，省部级杰青......",
+"照片地址":"https://faculty.ecnu.edu.cn/_upload/article/images/3a/5b/afaa6191485eb4ee90f4bba8ab4b/4dbd2be0-83de-4336-a08a-5d9be5a5fd4f_s.png",
 "最高学历":"研究生",
 "最高学位":"博士",
-"职位":"副教授",
-"办公地点":"天津市南开区卫津路94号南开大学联合楼A703室",
-"科研论文":"1. Jinhan Li, Keqiang Xu, Fangming Liu, Youzeng Li, Yanfang Hu, Xijie Chen, Huan Wang*, Wence Xu, Youxuan Ni, Guoyu Ding, Tete Zhao, Meng Yu*, Wei Xie, Fangyi Cheng*. Hollow Hierarchical Cu2O-Derived Electrocatalysts Steering CO2 Reduction to Multi-Carbon Chemicals at Low Overpotentials. Advanced Materials 2023, 35, 2301127.\n2. Tete Zhao, Jinhan Li, Jiuding Liu, Fangming Liu, Keqiang Xu, Meng Yu*, Wence Xu, Fangyi Cheng*. Tailoring the Catalytic Microenvironment of Cu2O with SiO2 to Enhance C2+ Product Selectivity in CO2 Electroreduction. ACS Catalysis 2023, 13, 4444.\n3. Tete Zhao, Xupeng Zong, Jiuding Liu, Jialei Chen, Keqiang Xu, Xiao Wang, Xijie Chen, Wutong Yang, Fangming Liu, Meng Yu*, Fangyi Cheng*. Functionalizing Cu nanoparticles with fluoric polymer to enhance C2+ roduct selectivity in membraned CO2 reduction. Applied Catalysis B: Environmental 2024, 340, 123281.\n4. Meng Yu#, Fangming Liu#, Jinhan Li, Jiuding Liu, Yudong Zhang, Fangyi Cheng*. Multidimensional nonstoichiometric electrode materials for electrochemical energy conversion and storage. Advanced Energy Materials 2021, 2100640.\n5. Meng Yu, Jinhan Li, Fangming Liu, Jiuding Liu, Wence Xu, Honglu Hu, Xijie Chen, Weichao Wang, Fangyi Cheng*. Anionic formulation of electrolyte additive towards stable lectrocatalytic oxygen evolution in seawater splitting. Journal of Energy Chemistry 2022, 72, 361."
+"职位":"教授",
+"办公地点":"上海市东川路500号 华东师范大学信息楼",
+"科研论文":"1. Guangdi Feng#, Qiuxiang Zhu#, Xuefeng Liu, Luqiu Chen, Xiaoming Zhao, Jianquan Liu, Shaobing Xiong, Kexiang Shan, Zhenzhong Yang, Qinye Bao, Fangyu Yue, Hui Peng, Rong Huang, Xiaodong Tang, Jie Jiang, Wei Tang, Xiaojun Guo, Jianlu Wang, Anquan Jiang, Brahim Dkhil, Bobo Tian*, Junhao Chu and Chungang Duan. A ferroelectric fin diode for robust non-volatile memory, Nature Communications, 15, 513 (2024). https://doi.org/10.1038/s41467-024-44759-5 \n2.  Guangjian Wu#, Xumeng Zhang#, Guangdi Feng#, Jingli Wang, Keji Zhou,  Jinhua Zeng, Danian Dong, Fangduo Zhu, Chenkai Yang, Xiaoming Zhao,  Danni Gong, Mengru Zhang, Bobo Tian*, Chungang Duan, Qi  Liu*, Jianlu Wang*, Junhao Chu and Ming Liu. Ferroelectric-defined  reconfigurable homojunctions for in-memory sensing and computing, Nature Materials, 22, 1499 (2023). https://www.nature.com/articles/s41563-023-01676-0\n3. Jie Lao#, Mengge Yan#, Bobo Tian*, Chunli  Jiang, Chunhua Luo, Zhuozhuang Xie, Qiuxiang Zhu, Zhiqiang Bao, Ni  Zhong, Xiaodong Tang, Linfeng Sun, Guangjian Wu, Jianlu Wang, Hui Peng*,  Junhao Chu, and Chungang Duan*. Ultralow-Power Machine Vision with  Self-Powered Sensor Reservoir, Advanced Science, 9 (15), 2106092 (2022). (高被引) https://onlinelibrary.wiley.com/doi/full/10.1002/advs.202106092\n4. Guangjian Wu#, Bobo Tian#, Lan Liu#, Wei Lv, Shuang Wu, Xudong Wang, Yan Chen, Jingyu Li, Zhen Wang, Shuaiqin Wu,Hong Shen, Tie Lin, Peng Zhou*, Qi Liu, Chungang Duan, Shantao Zhang, Xiangjian Meng, Shiwei Wu, Weida Hu, Xinran Wang, Junhao Chu and Jianlu Wang*. Programmable transition metal dichalcogenide homojunctions controlled by nonvolatile ferroelectric domains, Nature Electronics, 3, 43 (2020) (高被引)  https://www.nature.com/articles/s41928-019-0350-y\n5. Bobo Tian, Jianlu Wang*, Stephane Fusil, Yang Liu, Xiaolin Zhao, Shuo Sun, Hong Shen, Tie Lin, Jinglan Sun, Chungang Duan*, Manuel Bibes, Agnes Barthélémy, Brahim Dkhil, Vincent Garcia*, Xiangjian Meng and Junhao Chu. Tunnel electroresistance through organic ferroelectrics. Nature Communications 7, 11502 (2016) https://www.nature.com/articles/ncomms11502"
 }
 
 data = {
