548a3fda1ddc235e97a737cd1932020d29bda5c0
Tb2019 <982148620@qq.com>
2024-06-19 09:23:35 +0800

    南昌大学测试

--- 手动保存csv至数据库.py
+++ 手动保存csv至数据库.py
@@ -6,10 +6,10 @@ from asyncio.log import logger
 from utils import csv_2_df, truncate_table, df2mysql, drop_duplicate_collage, local_engine, sf_engine, save_as_json
 from urllib import parse
 
-school_id = 83
-college_id = 154
-school_name = '山东大学'
-college_name = '电气工程学院'
+school_id = 17
+college_id = 155
+school_name = '南昌大学'
+college_name = '软件学院'
 
 img_url_head = ''
 
