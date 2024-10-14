4af60dae1c38689cd62b6b0db01f94dd270dece5
Tb2019 <982148620@qq.com>
2024-10-14 09:37:59 +0800

    正常提交结果

--- 手动保存csv至数据库.py
+++ 手动保存csv至数据库.py
@@ -7,33 +7,33 @@ from utils import csv_2_df, truncate_table, df2mysql, drop_duplicate_collage, lo
 from urllib import parse
 
 school_id = 93
-college_id = 363
+college_id = 366
 school_name = '南开大学'
-college_name = '化学学院'
+college_name = '环境科学与工程学院'
 
 img_url_head = ''
 
 data_real = {
-"姓名": "于勐",
-"电话": "",
-"邮箱": "nkyu2023@nankai.edu.cn",
-"职称": "副教授",
-"个人简介": "于勐，博士，副教授，应用化学与工程研究所，毕业于南开大学，天津人。",
-"教育经历": "2009年9月 − 2013年7月，上海交通大学电子信息与电气工程学院，本科\n2013年8月 − 2015年5月，美国南加州大学维特比工程学院，硕士\n2015年6月 − 2019年6月，南开大学电子信息与光学工程学院，博士",
-"工作经历": "2019年7月 – 2022年6月，南开大学化学学院，博士后\n2022年7月 – 2023年9月，南开大学电子信息与光学工程学院，博士后\n2023年10月至今，南开大学化学学院，副教授",
-"研究方向": "能源材料化学，电催化材料与器件",
+"姓名": "赵强",
+"电话": "022-83662855",
+"邮箱": "qiangzhao@nankai.edu.cn",
+"职称": "教授",
+"个人简介": "博士，南开大学生命科学学院暨药物化学生物学国家重点实验室教授（博士生导师），教育部生物活性材料重点实验室副主任，天津市重点实验室主任。先后获得国家杰出青年科学基金（2019）、国家优秀青年科学基金（2015）和天津市杰出青年科学基金（2018）资助。入选天津市131创新型人才第一层次。担任中国生物医学工程学会理事、中国解剖学会血管分会常委、中国生物医学工程学会干细胞工程技术分会常委、中国生理学会基质生物学分会委员（青委会主委）、Fundamental Research编委、Engineered Regeneration 副主编等。",
+"教育经历": "2004/3 - 2006/9，天津大学，材料学，博士 \n2001/9 - 2004/2，天津大学，材料学，硕士 \n1997/9 - 2001/7，西北工业大学，高分子材料与工程，学士",
+"工作经历": "2014/12 - 至今， 南开大学，生命科学学院，教授 \n2009/11 – 2014/12，南开大学，生命科学学院，副教授 \n2014/3 - 2014/4， 英国伦敦大学国王学院，医学院，访问学者 \n2006/9 - 2009/9， 香港城市大学，物理与材料科学系，博士后研究员",
+"研究方向": "主要从事心血管生物材料与再生医学研究，利用工程材料科学、化学生物学等新技术、新方法，开展交叉研究，发展用于心血管修复再生与心血管疾病治疗的新材料与新技术。主持国家重点研发计划等省部级以上科研项目10余项。在国际权威学术期刊Nat Chem Biol、Nat Commun、Cell Rep、Adv Mater、Cir Res、Adv Sci、J Am Soc Nephrol、Biomaterials、J Control Release等发表SCI收录论文90余篇，论文他人引用>4000次，学术影响h-指数=38。在国际权威出版机构RSC、Springer等出版著作章节6部。申请并获得授权发明多项。先后获得天津市自然科学一等奖（2019，2/11）和天津市科技进步二等奖（2016，1/8；2021，5/8）。",
 "人才称号": "",
 "行政称号": "",
-"专利": "获发明专利授权专利1项",
-"科研项目": "作为项目负责人或骨干成员承担国家自然科学基金青年项目、国家重点研发计划“催化科学”专项等科研任务。",
-"荣誉/获奖": "2022年天津市自然科学二等奖（第二完成人）\n第二十次全国电化学大会优秀论文奖\nJournal of Energy Chemistry2022年作者交流会最佳报告奖",
-"照片地址": "https://chem.nankai.edu.cn/_upload/article/images/df/0f/38a6228f424cb1595751e78a505d/3263b494-6b8c-452d-9e90-c1fb364996f5.jpg",
+"专利": "",
+"科研项目": "1. 国家杰出青年科学基金项目“心血管生物材料” (项目批准号：81925021)，项目负责人\n2. 国家重点研发计划-战略性国际科技创新合作重点专项“基于化学生物学‘凸凹互补’原理的工程化酶的理性设计与医学应用” (项目批准号：2018YFE0200500)，项目负责人\n3. 国家优秀青年科学基金项目“心血管生物材料” (项目批准号：81522023)，项目负责人\n4. 国家自然科学基金创新研究群体项目“组织器官损伤的修复与再生” (项目批准号：81921004)，项目骨干\n5. 国家自然科学基金重大研究计划（培育项目）“通过仿生天然细胞外基质构建功能型人工血管促进组织再生重构” (项目批准号：91639113)，项目负责人\n6. 国家自然科学基金（面上项目）“基于化学生物学“凸凹互补”原理构建靶向一氧化氮递送系统用于缺血性疾病治疗” (项目批准号：81871500)，项目负责人\n7. 国家自然科学基金（面上项目）“具有微/纳米复合纤维结构的小口径人工血管构建及对组织再生的引导作用” (项目批准号：81371699)，项目负责人\n8. 国家自然科学基金（青年基金）“人工血管材料的多重功能化修饰及空间分布调控” (项目批准号：81000680)，项目负责人\n9. 国家自然科学基金（国际（地区）合作与交流项目）“用于血管再生的新型生物材料研究” (项目批准号：81310108034)，项目负责人\n10. 高等学校博士学科点专项科研基金（新教师类）“活性人工血管的构建和功能评价”(项目批准号：20100031120021)， 项目负责人\n11. 天津市杰出青年科学基金“心血管生物材料” (项目批准号：18JCJQJC46900)，项目负责人\n12. 天津市应用基础与前沿技术研究计划（自然科学基金）青年项目资金“通过可控降解和功能梯度改善细胞向支架材料内生长” (项目批准号：12JCQNJC09300)， 项目负责人\n13. 分子科学国家实验室开放基金“具有可控释放一氧化氮（NO）功能的心血管生物材料研究”，项目",
+"荣誉/获奖": "国家杰出青年科学基金（2019）\n国家优秀青年科学基金（2015）\n天津市杰出青年科学基金（2018）\n天津市“131人才”第一层次（2018）\n南开大学“百名青年学科带头人”（2016）\n南开大学第七届“良师益友”奖（2016）",
+"照片地址": "https://sky.nankai.edu.cn/_upload/article/images/29/9c/2532c7f94dd9a4b29918d126efcb/31db881c-4dc3-464a-9a61-767353f42926_s.jpg",
 "最高学历": "研究生",
 "最高学位": "博士",
-"职位": "副教授",
-"办公地点": "天津市南开区卫津路94号南开大学联合楼A703室",
-"科研论文": "1. Jinhan Li, Keqiang Xu, Fangming Liu, Youzeng Li, Yanfang Hu, Xijie Chen, Huan Wang*, Wence Xu, Youxuan Ni, Guoyu Ding, Tete Zhao, Meng Yu*, Wei Xie, Fangyi Cheng*. Hollow Hierarchical Cu2O-Derived Electrocatalysts Steering CO2 Reduction to Multi-Carbon Chemicals at Low Overpotentials. Advanced Materials 2023, 35, 2301127.\n2. Tete Zhao, Jinhan Li, Jiuding Liu, Fangming Liu, Keqiang Xu, Meng Yu*, Wence Xu, Fangyi Cheng*. Tailoring the Catalytic Microenvironment of Cu2O with SiO2 to Enhance C2+ Product Selectivity in CO2 Electroreduction. ACS Catalysis 2023, 13, 4444.\n3. Tete Zhao, Xupeng Zong, Jiuding Liu, Jialei Chen, Keqiang Xu, Xiao Wang, Xijie Chen, Wutong Yang, Fangming Liu, Meng Yu*, Fangyi Cheng*. Functionalizing Cu nanoparticles with fluoric polymer to enhance C2+ roduct selectivity in membraned CO2 reduction. Applied Catalysis B: Environmental 2024, 340, 123281.\n4. Meng Yu#, Fangming Liu#, Jinhan Li, Jiuding Liu, Yudong Zhang, Fangyi Cheng*. Multidimensional nonstoichiometric electrode materials for electrochemical energy conversion and storage. Advanced Energy Materials 2021, 2100640.\n5. Meng Yu, Jinhan Li, Fangming Liu, Jiuding Liu, Wence Xu, Honglu Hu, Xijie Chen, Weichao Wang, Fangyi Cheng*. Anionic formulation of electrolyte additive towards stable lectrocatalytic oxygen evolution in seawater splitting. Journal of Energy Chemistry 2022, 72, 361.",
-"源地址": "https://chem.nankai.edu.cn/2019/0906/c24393a540862/page.htm"
+"职位": "教授",
+"办公地点": "天津市南开区卫津路南开大学分子生物学研究所209",
+"科研论文": "1. Hou J, Pan Y, Zhu D, Fan Y, Feng G, Wei Y, Wang H, Qin K, Zhao T, Yang Q, Zhu Y, Che Y, Liu Y, Cheng J*, Kong D, Wang PG, Shen J*, Zhao Q*. Targeted delivery of nitric oxide via a “bump-and-hole”-based enzyme-prodrug pair. Nat Chem Biol 2019, 15: 151-60 (recommended by F1000).\n2. Zhu D, Hou J, Qian M, Jin D, Hao T, Pan Y, Wang H, Wu S, Liu S, Wang F, Wu L, Zhong Y, Yang Z, Che Y, Shen J, Kong D, Yin M*, Zhao Q*. Nitrate-functionalized patch confers cardioprotection and improves heart repair after myocardial infarction via local nitric oxide delivery. Nat Commun 2021, 12: 4501.\n3. Wang F, Qin K, Wang K, Wang H, Liu Q, Qian M, Chen S, Sun Y, Hou J, Wei Y, Hu Y, Li Z, Xu Q*, Zhao Q*. Nitric oxide improves regeneration and prevents calcification in bio-hybrid vascular grafts via regulation of vascular stem/progenitor cells. Cell Rep 2022, 39:110981.\n4.  Midgley AC, Wei Y, Li Z, Kong D, Zhao Q*. Nitric oxide–releasing biomaterial regulation of the stem cell microenvironment in regenerative medicine. Adv Mater 2020, e1805818.\n5.  Hao T, Qian M, Zhang Y, Liu Q, Midgley AC, Liu Y, Che Ye, Hou J, Zhao Q*. An injectable dual-function hydrogel protects against myocardial ischaemia/reperfusion injury by modulating ROS/NO disequilibrium. Adv Sci 2022, 9: 2105408. ",
+"源地址": "https://sky.nankai.edu.cn/zq1/list.htm"
 }
 
 data = {
